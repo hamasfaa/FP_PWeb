e6051ef91494f59b003b6766a29150b2e5ee3b96
Hamasah Fatiy Dakhilullah <hamasfa@gmail.com>
2024-11-17 15:55:17 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,php}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        modak: ['Modak', 'cursive'],
+        poppins: ['Poppins', 'sans-serif']
+      },
+      colors: {
+        teal: '#008080',
+        'light-teal': '#20B2AA',
+        'dark-teal': '#006666',
+        'teal-blue': '#367588',
+        'teal-green': '#128C7E',
+      },
+    },
+  },
+  plugins: [],
+}
