431b747f7825c7c5f4c06f4c98656550490e1382
Hamasah Fatiy Dakhilullah <hamasfa@gmail.com>
2024-12-11 09:06:55 +0700

    fix bug

--- src/dosen/index.php
+++ src/dosen/index.php
@@ -556,6 +556,7 @@ $stmt_listPertemuan->close();
             const hours = String(timeNow.getHours()).padStart(2, '0');
             const minutes = String(timeNow.getMinutes()).padStart(2, '0');
             const seconds = String(timeNow.getSeconds()).padStart(2, '0');
+            document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
         }
         setInterval(updateClock, 1000);
         updateClock();
